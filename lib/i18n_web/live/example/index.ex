cf14766d90b247ec91353555a2b4b00bd73d453f
Dean Kinyua <kinyuadean@gmail.com>
2025-06-09 20:12:55 +0300

    use cookies to store locales

--- lib/i18n_web/live/example/index.ex
+++ lib/i18n_web/live/example/index.ex
@@ -9,13 +9,14 @@ defmodule I18nWeb.ExampleLive.Index do
   def render(assigns) do
     ~H"""
     <div class="flex items-center justify-center h-screen">
-      <h2 class="text-4xl">{gettext("Only one difference when it comes to LiveViews")}</h2>
+      <h2 class="text-4xl">{gettext("Hello World")}</h2>
     </div>
     """
   end
 
   @impl Phoenix.LiveView
-  def mount(_params, _session, socket) do
+  def mount(%{"locale" => locale} = _params, _session, socket) do
+    Gettext.put_locale(I18nWeb.Gettext, locale)
     {:ok, socket}
   end
 end
--- lib/i18n_web/plugs/locale_plug.ex
+++ lib/i18n_web/plugs/locale_plug.ex
@@ -6,9 +6,26 @@ defmodule I18nWeb.Plugs.Locale do
   def init(_opts), do: nil
 
   def call(%Plug.Conn{params: %{"locale" => locale}} = conn, _opts) when locale in @locales do
-    I18nWeb.Gettext |> Gettext.put_locale(locale)
-    conn
+    case fetch_locale_from(conn) do
+      nil ->
+        conn
+
+      locale ->
+        I18nWeb.Gettext |> Gettext.put_locale(locale)
+        conn |> put_resp_cookie("locale", locale, max_age: 365 * 24 * 60 * 60)
+    end
   end
 
   def call(conn, _opts), do: conn
+
+  defp fetch_locale_from(conn) do
+    dbg(conn.cookies)
+
+    (conn.params["locale"] || conn.cookies["locale"])
+    |> check_locale()
+  end
+
+  defp check_locale(locale) when locale in @locales, do: locale
+
+  defp check_locale(_), do: nil
 end
